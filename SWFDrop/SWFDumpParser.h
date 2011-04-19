1281987f9071a479de8c2fc69cab52c4278fca98
Marcel M <marcelm@amcoonline.net>
2011-04-19 15:50:48 -0700

    Added gitignores

---
+++ SWFDrop/SWFDumpParser.h
@@ -0,0 +1,17 @@
+//
+//  SWFDumpParser.h
+//  SWFDrop
+//
+//  Created by Marcel Miranda on 4/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SWFDumpParser : XMLParser {
+@private
+    
+}
+
+@end
---
+++ SWFDrop/SWFDumpParser.m
@@ -0,0 +1,29 @@
+//
+//  SWFDumpParser.m
+//  SWFDrop
+//
+//  Created by Marcel Miranda on 4/19/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SWFDumpParser.h"
+
+
+@implementation SWFDumpParser
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
